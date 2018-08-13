c11593706ca5fefc79c30693a7b733f8c9e086d0
xin-LianK <350824110@qq.com>
2018-08-13 15:03:23 +0800

    --

--- src/app/form-test/form-test.module.ts
+++ src/app/form-test/form-test.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 import { FormTestComponent } from './form-test.component';
 import { Ng2ValidationComponent } from './ng2-validation/ng2-validation.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CustomFormsModule } from 'ng2-validation';
+import { FormTestRoutes } from './form-test.routing';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormTestRoutes,
+    ReactiveFormsModule,
+    FormsModule,
+    CustomFormsModule,
   ],
   declarations: [
     FormTestComponent,
