4b544b1b4f136147d5909a16c0b5ba8e840c0b58
xin-LianK <350824110@qq.com>
2018-09-14 15:01:39 +0800

    移动端rem

--- src/app/home/index/index.component.ts
+++ src/app/home/index/index.component.ts
@@ -10,6 +10,11 @@ export class IndexComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const c = document.getElementById('myCanvas') as HTMLCanvasElement;
+    const ctx = c.getContext('2d');
+    ctx.beginPath();
+    ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+    ctx.stroke();
   }
 
 }
