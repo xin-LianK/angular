e1b98ffb4d9a3c37454ccf5c86ee947d19d49982
xin-LianK <350824110@qq.com>
2018-09-21 10:45:11 +0800

    --

--- src/app/gps/index/index.component.ts
+++ src/app/gps/index/index.component.ts
@@ -1,39 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, filter, mergeMap } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-index',
-  templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+    selector: 'app-index',
+    templateUrl: './index.component.html',
+    styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
-  sessionId: Observable<string>;
-  token: Observable<string>;
+    sessionId: Observable<string>;
+    token: Observable<string>;
 
-  constructor(
-    private title: Title,
-    private route: ActivatedRoute,
-    private router: Router
+    constructor(
+        private title: Title,
+        private route: ActivatedRoute,
+        private router: Router
 
-  ) {
-    this.route.data.subscribe(t => title.setTitle(t['title']));
-    this.router.events.subscribe(r => {
-      console.log(r);
-    });
-  }
+    ) {
+        this.route.data.subscribe(t => title.setTitle(t['title']));
+        this.router.events.subscribe((r) => {
+          // example: NavigationStart, RoutesRecognized, NavigationEnd
+            if (r instanceof NavigationEnd) {
+                // 当导航成功结束时执行
+            }
+        });
+    }
 
-  ngOnInit() {
-    // Capture the session ID if available
-    this.sessionId = this.route
-      .queryParamMap
-      .pipe(map(params => params.get('session_id') || 'None'));
+    ngOnInit() {
+        // Capture the session ID if available
+        this.sessionId = this.route
+            .queryParamMap
+            .pipe(map(params => params.get('session_id') || 'None'));
 
-    // Capture the fragment if available
-    this.token = this.route
-      .fragment
-      .pipe(map(fragment => fragment || 'None'));
-  }
+        // Capture the fragment if available
+        this.token = this.route
+            .fragment
+            .pipe(map(fragment => fragment || 'None'));
+    }
 }
